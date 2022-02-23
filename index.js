55ecee53115f7bb63b572845275557cb8a8ada08
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-02-22 17:19:16 -0800

    commit tree eaf04bb0f71c693d4dbeb753c422521c7b191f11

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Document is ready to be manipulated");
+    const p = document.getElementById('text')
+    p.textContent = 'This is really cool!'
+});
+
+console.log("This console.log() fires when index.js loads - before DOMContentLoaded triggered.");
